008a9997588d3447689050a3d3473d09f717ca92
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-08-19 18:50:41 +0530

    build error fix

--- client/src/auth-config.ts
+++ client/src/auth-config.ts
@@ -1,6 +1,6 @@
 const config = {
-	domain: import.meta.env.VITE_DOMAIN,
-	clientId: import.meta.env.VITE_CLIENTID
+	domain: import.meta.env.VITE_DOMAIN || '',
+	clientId: import.meta.env.VITE_CLIENTID || ''
 };
 
 export default config;
